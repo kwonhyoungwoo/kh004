42909f4df2172a50885543bac39ac419cbfb3426
kwonhyoungwoo <cruel470@naver.com>
2022-10-21 18:52:25 +0900

    reinit

---
+++ src/main/java/com/example/kuly2/member/request/MemberLoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.kuly2.member.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberLoginRequest {
+
+	private String id;
+	private String password;
+}
---
+++ src/main/resources/static/js/member.js
@@ -0,0 +1,35 @@
+
+function joinCheck() {
+	if (document.frm.id.value.length == 0) {
+		alert("아이디를 써주세요");
+		frm.id.focus();
+		return false;
+	}
+	
+	if (document.frm.id.value.length < 4) {
+		alert("아이디는 4글자이상이어야 합니다.");
+		frm.id.focus();
+		return false;
+	}
+	if (document.frm.password.value == "") {
+		alert("암호는 반드시 입력해야 합니다.");
+		frm.pwd.focus();
+		return false;
+	}
+	if (document.frm.password.value != document.frm.pwd2.value) {
+		alert("암호가 일치하지 않습니다.");
+		frm.pwd.focus();
+		return false;
+	}
+	
+	if (document.frm.name.value.length == 0) {
+		alert("이름을 써주세요.");
+		frm.name.focus();
+		return false;
+	}
+	
+	
+	
+	
+	return true;
+}
