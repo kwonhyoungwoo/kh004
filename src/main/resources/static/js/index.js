7a5c4d23927ad574e57604ac467a7d4f99b6f655
YUNSEO27 <4876049@naver.com>
2022-10-22 23:49:19 +0900

    업데이트
    +메인페이지
    +CSS
    +JS 추가

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function(){
+	
+	var $slideWrap = document.querySelector('.slide_area'),
+		$slideContainer = document.querySelector('.slider_container'),
+		$slide = document.querySelectorAll('.slide'),
+		$navPrev = document.getElementById('prev'),
+		$slideHeight = 0,
+		$slideCount = $slide.length,
+		$currentIndex = 0,
+		$navNext = document.getElementById('next');
+		
+		//슬라이드 높이 확인 부모 높이 지정
+		for(var i = 0; i < $slideCount; i++){
+			if($slideHeight < $slide[i].offsetHeight){
+				$slideHeight = $slide[i].offsetHeight;
+			}
+		}
+	
+		$slideWrap.style.height = $slideHeight + 'px';
+		$slideContainer.style.height = $slideHeight + 'px';
+		
+		//슬라이드 있으면 가로로 배열하기 
+		for(var a = 0; a < $slideCount; a++){
+			$slide[0].style.left = a * 100 + '%';
+		}
+		
+		//슬라이드 이동 함수
+		function goToSlide(idx){
+			$slideContainer.style.left = -100 * idx + '%';
+			$currentIndex = idx;
+		}
+		
+		//다음, 이전버튼을 클릭하면 할일.
+		$navPrev.addEventListener('click', function(){
+			
+			if($currentIndex == 0){
+				goToSlide($slideCount -1);
+			} else {
+				goToSlide($currentIndex -1);
+			}
+		});
+		
+		$navNext.addEventListener('click', function(){
+			
+			if($currentIndex == $slideCount -1){
+				goToSlide(0);
+			} else {
+				goToSlide($currentIndex + 1);
+			}
+		
+		});
+		
+		//자동 슬라이드
+		setInterval(function(){
+			var nextIdx = ($currentIndex + 1) % $slideCount; 	
+			goToSlide(nextIdx);	
+		}, 4000);
+});
---
+++ src/main/webapp/WEB-INF/views/main/index.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%-- 헤더 파일 include --%>
+<%@include file ="../layouts/header.jsp" %>
+<%-- 슬라이드 영역 --%>
+	<div class="slide_area">
+		<ul class="slider_container simple-list" id="slider">
+			<li class="slide"><img src="/images/slide_img_1.png"></li>
+			<li class="slide"><img src="/images/slide_img_2.png"></li>	
+		</ul>			
+		<a href="#" id="prev"><i class="fa fa-caret-left fa-3x"></i></a>
+		<a href="#" id="next"><i class="fa fa-caret-right fa-3x"></i></a>
+	</div>
+<%-- 상품리스트 --%>
+<div id="contents">
+	<h3>지금 가장 핫한 상품<i class="fa fa-chevron-right"></i></h3>
+	<div class="best_list">
+	<%--반복문으로 상품 리스트 출력 --%>
+	<div class="item">
+		<a href="#">
+		<div class="img_box">
+			<img src="/images/123.png">
+		</div>
+		<div class="txt_box">
+		  <h4>[HBAF] 매일 색다른 </h4>    
+          <p>15,000 원</p>
+        </div>  
+		</a>
+	</div>
+	<%--끝나는 위치 --%>
+	</div>
+	<h3>이 상품 어때요?</h3>
+	<div class="another_list">
+	<%--반복문으로 상품 리스트 출력 --%>
+	<div class="item">
+		<a href="#">
+		<div class="img_box">
+			<img src="/images/123.png">
+		</div>
+		<div class="txt_box">
+		  <h4>name</h4>    
+          <p>price</p>
+        </div>  
+		</a>
+	</div>
+	</div>
+</div>
+<%-- 푸터 파일 include --%>
+<%@include file ="../layouts/footer.jsp" %>
+
