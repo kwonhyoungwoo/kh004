167451f0091729b1b475c760092884d526f5df08
hellfirewar <hellfire_war@nate.com>
2022-11-09 23:30:56 +0900

    no message

--- src/main/java/com/example/kuly2/WebConfig.java
+++ src/main/java/com/example/kuly2/WebConfig.java
@@ -1,20 +1,14 @@
 package com.example.kuly2;
 
-import com.example.kuly2.admin.security.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-	private final LoginUserArgumentResolver loginUserArgumentResolver;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -23,9 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
 			.setCacheControl(CacheControl.noCache());
 	}
 
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-		argumentResolvers.add(loginUserArgumentResolver);
-	}
 
 }
